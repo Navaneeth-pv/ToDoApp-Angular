1b96f5ed19a5ef01580e78a4041bad863dd416f4
Navaneeth-pv <navaneethpv902@gmail.com>
2022-11-25 12:43:08 +0530

    viewtodo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddToDoComponent } from './add-to-do/add-to-do.component';
+import { ViewAllToDoComponent } from './view-all-to-do/view-all-to-do.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddToDoComponent
+    AddToDoComponent,
+    ViewAllToDoComponent
   ],
   imports: [
     BrowserModule,
